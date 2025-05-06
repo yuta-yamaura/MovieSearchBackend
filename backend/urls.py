30f51ce206f0fbcacc584a8304eac4acb1d4a3aa
yuta-yamaura <shk0fate@gmail.com>
2025-05-07 00:21:44 +0900

    backend/urls.pyのbaseのimportを修正

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from base import views
 from movie_search import views
 from django.views.generic import TemplateView
 
