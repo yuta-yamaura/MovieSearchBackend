4aa8565cb572c279a730abb7f2c7f26fdc3faf3e
yuta-yamaura <shk0fate@gmail.com>
2025-05-06 20:00:58 +0900

    映画の情報を日本語で取得できるよう修正

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.getRoutes, name='getRoutes'),
     path('movies/<int:movie_id>/', views.get_movie_details, name='movie_details'),
 ]
--- base/views.py
+++ base/views.py
@@ -12,28 +12,8 @@ env.read_env('.env')
 TMDB_API_KEY = env('TMDB_API_KEY')
 
 @api_view(['GET'])
-@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
-def getRoutes(request):
-    routes = [
-        'api/products/',
-        'api/products/create',
-        'api/products/upload',
-        'api/products/<id>/reviews',
-        'api/products/top/',
-        'api/products/<id>/reviews',
-        'api/products/delete/<id>',
-        'api/products/<id>/reviews',
-        'api/products/update/<id>',
-        'api/products/<id>/reviews',
-        'api/products/delete/<id>',
-        'api/products/<id>/reviews',
-        'api/products/delete/<id>',
-    ]
-    return Response(routes)
-
-@api_view(['GET'])
 def get_movie_details(request, movie_id):
-    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}'
+    url = f'https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=ja-JP'
     print(url)
     response = requests.get(url)
     print(response)
