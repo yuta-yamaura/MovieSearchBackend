d537bd8ba6dec24959b6cb6cb29fd9f6371163f2
yuta-yamaura <shk0fate@gmail.com>
2025-05-07 09:32:01 +0900

    静的ファイルを参照できるようsettings.pyとurls.pyを修正

--- backend/settings.py
+++ backend/settings.py
@@ -127,9 +127,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend', 'dist'),
+    os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
 ]
 
+# WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'frontend', 'dist')
+WHITENOISE_INDEX_FILE = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
