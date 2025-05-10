92334fa9f03f3ab3eff540ab5713c3f638fb8990
yuta-yamaura <shk0fate@gmail.com>
2025-05-10 17:29:30 +0900

    TMDBの環境変数をsettings.pyから呼び出すように修正

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+# Initialize environ
+env = environ.Env()
+env.read_env('.env')
+TMDB_API_KEY = env('TMDB_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- movie_search/views.py
+++ movie_search/views.py
@@ -1,15 +1,8 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-import environ
+from backend.settings import TMDB_API_KEY
 # Create your views here.
-
-env = environ.Env()
-env.read_env('.env')
-TMDB_API_KEY = env('TMDB_API_KEY')
 
 @api_view(['GET'])
 def get_movie_details(request):
