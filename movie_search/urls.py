3c155043ccf781ea9b189d9cce7614d8ee1be0ba
yuta-yamaura <shk0fate@gmail.com>
2025-05-15 17:45:48 +0900

    年月検索をRESTに修正

--- movie_search/urls.py
+++ movie_search/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movies/', views.get_movie_details, name='movie_details'),
+    path('movies/', views.get_movie_details, name='movie_list'),
+    path('movies/<int:year>/', views.get_movie_year, name='movie_year'),
+    path('movies/<int:year>/<str:query>/', views.get_movie_query, name='movie_query'),
 ]
--- movie_search/views.py
+++ movie_search/views.py
@@ -2,35 +2,35 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
 from backend.settings import TMDB_API_KEY
+from urllib.parse import unquote
+
 # Create your views here.
 
 @api_view(['GET'])
 def get_movie_details(request):
     page = request.GET.get('page', 1)  # クエリパラメータからページ番号を取得
-    year = request.GET.get('year', 2024) # 検索用のリリース年
-    query = request.GET.get('query', '') # 検索キーワード
+    year = request.GET.get('year', 2023) # 検索用のリリース年
     
-    # 検索クエリがある場合はsearch/movieエンドポイントを使用
-    if query:
-        url = f'https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=ja-JP&page={page}&query={query}'
-        response = requests.get(url)
-        data = response.json()
-        
-        # 検索結果を年でフィルタリング
-        filtered_results = [
-            movie for movie in data.get('results', [])
-            if movie.get('release_date', '').startswith(str(year))
-        ]
-        
-        # フィルタリング後の結果を新しいレスポンスとして返す
-        return Response({
-            'page': data.get('page', 1),
-            'results': filtered_results,
-            'total_results': len(filtered_results),
-            'total_pages': data.get('total_pages', 1)
-        })
-    else:
-        # 検索クエリがない場合は通常のdiscover/movieエンドポイントを使用
-        url = f'https://api.themoviedb.org/3/discover/movie?api_key={TMDB_API_KEY}&language=ja-JP&page={page}&primary_release_date.gte={year}-01-01&primary_release_date.lte={year}-12-31&sort_by=popularity.desc'
-        response = requests.get(url)
-        return Response(response.json())
+    # 検索クエリがない場合は通常のdiscover/movieエンドポイントを使用
+    url = f'https://api.themoviedb.org/3/discover/movie?api_key={TMDB_API_KEY}&language=ja-JP&page={page}&primary_release_date.gte={year}-01-01&primary_release_date.lte={year}-12-31&sort_by=popularity.desc'
+    response = requests.get(url)
+    return Response(response.json())
+
+@api_view(['GET'])
+def get_movie_year(request, year):
+    page = request.GET.get('page', 1)  # クエリパラメータからページ番号を取得
+    url = f'https://api.themoviedb.org/3/discover/movie?api_key={TMDB_API_KEY}&language=ja-JP&page={page}&primary_release_date.gte={year}-01-01&primary_release_date.lte={year}-12-31&sort_by=popularity.desc'
+    response = requests.get(url)
+    print(response.json())
+    return Response(response.json())
+
+@api_view(['GET'])
+def get_movie_query(request, year, query):
+    page = request.GET.get('page', 1)  # クエリパラメータからページ番号を取得
+    # URLの文字化けを修正
+    decoded_query = unquote(query)
+    print(decoded_query)
+    url = f'https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=ja-JP&query={decoded_query}&primary_release_year={year}&page={page}'
+    response = requests.get(url)
+    # print(response.json())
+    return Response(response.json())
